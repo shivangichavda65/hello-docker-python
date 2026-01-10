bd7920350a7c02e070b7160bc21f35bf74aa2e8a
shivangichavda65 <shivangichavda65@gmail.com>
2026-01-10 13:01:59 +0530

    initial update

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello v2 - Updated app!"
+    return "shivangi-bipasha"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
